c39b7c91367f24a46003aa581c44dc5b98d711ac
SeanLovesTea <sean.m.concannon@gmail.com>
2022-08-03 09:42:49 +0100

    latest shade attempt, it needs work still

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,8 @@
 const grid = document.querySelector('.grid');
 const gridBtn = document.querySelector('.gridBtn');
 const clearBtn = document.querySelector('.clear-squares');
-const rainbowBtn = document.querySelector('.rainbow')
+const rainbowBtn = document.querySelector('.rainbow');
+
 let color = "black";
 
 clearBtn.addEventListener('click', clearGrid);
@@ -25,7 +26,6 @@ function makeGrid(gridMultiplier){
         gridColumn.style.borderBottom = '0px';
         gridColumn.className ="gridCell"
         gridColumn.addEventListener('mouseover', colorSquare);
-        // gridColumn.addEventListener('mouseover', shade);
         grid.appendChild(gridColumn);
 
     for(let i= 0;i < gridMultiplier; i++){
@@ -36,7 +36,6 @@ function makeGrid(gridMultiplier){
         gridRow.style.border = '.2px solid lightgrey';
         gridRow.className ="gridCell"
         gridRow.addEventListener('mouseover', colorSquare);
-        // gridColumn.addEventListener('mouseover', shade);
         gridColumn.appendChild(gridRow);
         }
     }
@@ -55,125 +54,35 @@ function removeDivs(){
     return;
 }
 
-function colorSquare(){
+function colorSquare(e){
     if (color === "random") {
         this.style.backgroundColor =`hsl(${Math.random() * 360}, 100%, 50%)`;
+        console.log(this)
     }
     else if (color === 'shade'){
-        let num = 0 
-        if(true){
-            num = num + 20;
-            this.style.backgroundColor =`hsl(240, 0%, ${num + 20}%)`;
-        }
-    }
-        // else{
-        //    
-        // }
-        // this.style.backgroundColor = 'black'
-        // currentOpacity = this.style.opacity;
-        // if(currentOpacity){
-        //     this.style.opacity = Number(currentOpacity) +.1;
-        // }else
-        // this.style.opacity = .1;
-
-    else
-    {this.style.backgroundColor = color;}
+        const currentOpacity = this.style.opacity
+        console.log(currentOpacity)
+        this.style.backgroundColor = 'black'
+        if(currentOpacity){
+            this.style.opacity = Number(currentOpacity) +.1;
+        }else
+        this.style.opacity = .1;
+    };
+    this.style.backgroundColor = color
 };
-
-// function shade(){
-//     if (color === "shade"){
-//     const currentOpacity = this.style.opacity
-//     div.style.backgroundColor = 'black'
-//     //     if(currentOpacity){
-//     //         this.style.opacity = Number(currentOpacity) +.1;
-//     //     }else
-//     //     this.style.opacity = .1;
-//      }
-// }
-
+      
 function changeColor(choice){
     color = choice;
     console.log(color)
 }
-// let num = shadeNum()
-// function shadeNum(){
-//     let num = 0;
-//     if(num == 0){
-//         num = 20;
-//         return num
-//     }else if (num == 20){
-//         num = 40
-//         return num
-//     }else if (num == 40){
-//         num = 60
-//         return num
-//     }else if (num == 60){
-//         num = 80
-//         return num
-//     }else if (num == 80){
-//         num = 100
-//         return num
-//     }
-// };
 // function shade(){
-//     // div = grid.getElementsByClassName('.gridCell')
-//     if(color !== 'lightgrey'){
-//         shadeColor = 'lightgrey'
-//     //     div.style.backgroundColor = 'lightgrey'
-//     }else if(color == 'lightgrey'){
-//         shadeColor = 'grey'
-//     //     div.style.backgroundColor = 'grey'
-//     }else if(color == 'grey'){
-//         shadeColor = '#808080'
-//         // div.style.backgroundColor = '#808080'
-//     }else if(color == '#808080'){
-//         shadeColor = '#303030'
-//         // div.style.backgroundColor = '#303030'
-//     }else if(color == '#303030'){
-//         shadeColor = 'black'
-//         // div.style.backgroundColor = 'black'
-//     }		
-// }
-// function greyToBlack(){
-//     for (let i = 0; i < 5; i++){
-//         num + 10
-//         return `hsl(240%, 0%, ${num})`;
-//     }
-// }
-// console.log(color)`
-// rainbowBtn.addEventListener('click', ()=>{
-//     let color = randomRgbColor();
-//     colorSquare(color);
-    
-// });
-// function rainbow(gridColumn,gridRow){
-// gridRow.addEventListener('mouseover',rainbowSquare());
-// gridColumn.addEventListener('mouseover',rainbowSquare());
-// }
-// rainbowSquare()
-// function rainbowSquare(){
-//     let randomCol = randomRgbColor();
-//     this.style.backgroundColor = randomCol;
-//     console.log(randomCol)
-// }
-// function resetColor(gridColumn,gridRow){
-//     gridColumn.forEach((gridColumn), ()=> {
-//         gridColumn.backgroundColor = 'white'
-//     })
-//     gridRow.forEach((gridRow), ()=> {
-//         gridRow.backgroundColor = 'white'
-//     })
-// // }
-
-// function randomInteger(max) {
-//     return Math.floor(Math.random()*(max + 1));
-// }
-// randomRgbColor()
-// function randomRgbColor() {
-//     let r = randomInteger(255);
-//     let g = randomInteger(255);
-//     let b = randomInteger(255);
-//     console.log(`"rgb(${r},${g},${b})"`)
-//     randChoice = `"rgb(${r},${g},${b})"`
-//     return 
+//     let gridCells = grid.querySelectorAll('div')
+//     const currentOpacity = gridCells.style.opacity;
+//     gridCells.forEach((div) => div.style.backgroundColor = 'white');
+//     console.log(gridCells.opacity)
+//     gridCells.style.backgroundColor = 'black'
+//         if(currentOpacity){
+//             gridCells.style.opacity = Number(currentOpacity) +.1;
+//         }else
+//         gridCells.style.opacity = .1;
 // }
